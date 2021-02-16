791f9b3f2d1df659e31cee677b095c990a9fcb6f
Will B <willhbjr@gmail.com>
2021-02-16 04:39:43 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// Run this sample code as-is and examine the output. 
+// Should the shuttle have launched? 
+// Did it?
+// Do not worry about fixing the code yet, we will do that in the next series of exercises.
+
+let launchReady = false;
+let crewReady = false;
+let fuelLevel = 17000;
+let crewStatus = true;
+let computerStatus = 'green';
+
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+} //console.log(launchReady);
+
+if (crewStatus && computerStatus === 'green'){
+   console.log('Crew & computer cleared.');
+   crewReady = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   crewReady = false;
+} //console.log(crewReady);
+
+if (launchReady && crewReady) {
+   console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+   console.log('Liftoff!');
+} else {
+   console.log('Launch scrubbed.');
+}
